77219ae3f5e6aebe6880f268a93563d0ef4d6fde
Annie Sun <anniesun@Annies-MBP.lan>
2023-07-11 01:55:08 -0400

    init

---
+++ src/index.ts
@@ -0,0 +1,69 @@
+/* THIS IS A DEMO APP */
+import Keylime, {
+  useState,
+  element,
+  text,
+  useEffect,
+  KeylimeNode,
+} from "create-keylime";
+
+const DOM = document.getElementById("my-app");
+Keylime.setRoot(DOM);
+
+import "./style.css";
+
+function getRandomMovie(max: number) {
+  return Math.floor(Math.random() * max);
+}
+
+const MovieCard = (src: string, title: string): KeylimeNode => {
+  return element("div", { className: "movieCard" }, [
+    element(
+      "img",
+      {
+        className: "image",
+        src,
+      },
+      []
+    ),
+    element("h2", { className: "title" }, [text(title)]),
+  ]);
+};
+
+const myComponent = (): KeylimeNode => {
+  const [movies, setMovies] = useState(null);
+  const [index, setIndex] = useState(0);
+
+  useEffect(async () => {
+    const response = await fetch("https://ghibliapi.vercel.app/films");
+    const movies = await response.json();
+    console.log(movies);
+    setMovies(movies);
+    setIndex(getRandomMovie(movies.length));
+  }, []);
+
+  if (!movies) return null;
+
+  return element("div", { className: "container" }, [
+    element("h1", { className: "pageTitle" }, [text("Welcome to Keylime 👋")]),
+    element("div", { className: "taglineContainer" }, [
+      element("p", { className: "desc" }, [
+        text("Get a Ghibli movie to watch :)"),
+      ]),
+      element(
+        "u",
+        {
+          className: "linkButton",
+          on: {
+            click: () => setIndex(getRandomMovie(movies.length)),
+          },
+        },
+        [text("Click me")]
+      ),
+    ]),
+    MovieCard(movies[index].image, movies[index].title),
+    element("a", {className: "link", href:"https://github.com/annxiesun/keylime"}, [text("built with love from scratch ♡")])
+  ]);
+};
+
+Keylime.renderRoot(myComponent);
---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ESLintPlugin = require("eslint-webpack-plugin");
+
+module.exports = {
+  context: path.resolve(__dirname, "src"),
+  devtool: "inline-source-map",
+  entry: "./index.ts",
+  mode: "development",
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+      },
+      { test: /\.css$/, use: [ 'style-loader', 'css-loader' ] }
+    ],
+  },
+  output: {
+    filename: "client.js",
+    path: path.resolve(__dirname, "static/js"),
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".jsx", ".js"],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "our project",
+      template: "../public/index.html",
+    }),
+    new ESLintPlugin(),
+  ],
+};
